b3e1fbaa58a540cb424c94d07bda5981990e26dc
Daniil Reznikov <daniil@noorsoft-mobile.ru>
2022-09-13 21:03:49 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from "react";
+import "./styles/index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Counter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Counter.module.scss";
+
+const Counter = () => {
+  return <div className={styles.div}>Hello World</div>;
+};
+
+export default Counter;
