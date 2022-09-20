5ccbb8b464953a762ab9aaba9087870c5a77532d
Daniil Reznikov <daniil@noorsoft-mobile.ru>
2022-09-20 21:40:18 +0200

    add jest, first test

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -22,18 +22,6 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       exclude: /node_modules/,
       use: {
          loader: 'babel-loader',
-         options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-               [
-                  'i18next-extract',
-                  {
-                     locales: ['ru', 'en'],
-                     keyAsDefaultValue: true,
-                  },
-               ],
-            ],
-         },
       },
    };
 
