f8b4952f0f6800d9b2fa159247ba554a9866bcde
Daniil Reznikov <daniil@noorsoft-mobile.ru>
2022-09-20 16:24:43 +0200

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,11 +3,6 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-   const svgLoader = {
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-   };
-
    const fileLoader = {
       test: /\.(png|jpe?g|gif|woff2|woff)$/i,
       use: [
@@ -15,6 +10,31 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
             loader: 'file-loader',
          },
       ],
+   };
+
+   const svgLoader = {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+   };
+
+   const babelLoader = {
+      test: /\.(js|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+         loader: 'babel-loader',
+         options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+               [
+                  'i18next-extract',
+                  {
+                     locales: ['ru', 'en'],
+                     keyAsDefaultValue: true,
+                  },
+               ],
+            ],
+         },
+      },
    };
 
    const typescriptLoader = {
@@ -26,7 +46,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
    const cssLoader = {
       test: /\.s[ac]ss$/i,
       use: [
-      // Creates `style` nodes from JS strings
+         // Creates `style` nodes from JS strings
          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
          // Translates CSS into CommonJS
          {
@@ -43,5 +63,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       ],
    };
 
-   return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+   return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
