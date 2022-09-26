8e1ae8c1a346c8b028066f7f4d137056d00df601
Daniil Reznikov <daniil@noorsoft-mobile.ru>
2022-09-26 19:46:31 +0200

    add page loader, add pageError

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+
+// Компонент для тестирования
+export const BugButton: React.FC = () => {
+   const [error, setError] = useState(false);
+   const { t } = useTranslation();
+
+   useEffect(() => {
+      if (error) {
+         throw new Error();
+      }
+   }, [error]);
+   return (
+      <Button
+         onClick={() => { setError(true); }}
+      >
+         {t('Выбросить ошибку')}
+      </Button>
+   );
+};
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import styles from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: React.FC<PageErrorProps> = ({ className }) => {
+   const { t } = useTranslation();
+
+   const reloadPage = () => {
+      window.location.reload();
+   };
+
+   return (
+      <div className={classNames(styles.pageError, {}, [className])}>
+         <p>{t('Произошла непредвиденная ошибка')}</p>
+         <Button onClick={reloadPage}>
+            {t('Обновить страницу')}
+         </Button>
+      </div>
+   );
+};
