d3fc3bcb4f95f4bc11ef0c4566fe3bb5d69d3870
Daniil Reznikov <daniil@noorsoft-mobile.ru>
2022-10-01 12:08:49 +0200

    added stories

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
-import styles from './Navbar.module.scss';
+import React from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { useTranslation } from "react-i18next";
+import styles from "./Navbar.module.scss";
+import { Loader } from "shared/ui/Loader/Loader";
 
 interface NavbarProps {
-   className?: string;
+  className?: string;
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ className }) => {
-   const { t } = useTranslation();
+  const { t } = useTranslation();
 
-   return (
-      <div className={classNames(styles.Navbar, {}, [className])}>
-
-         <div className={styles.links}>
-            <AppLink
-               className={styles.mainLink}
-               theme={AppLinkTheme.SECONDARY}
-               to="/"
-            >
-               {t('Главная')}
-            </AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-               {t('О сайте')}
-            </AppLink>
-         </div>
+  return (
+    <div className={classNames(styles.Navbar, {}, [className])}>
+      <div className={styles.links}>
+        <AppLink
+          className={styles.mainLink}
+          theme={AppLinkTheme.SECONDARY}
+          to="/"
+        >
+          {t("Главная")}
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to="/about">
+          {t("О сайте")}
+        </AppLink>
       </div>
-   );
+    </div>
+  );
 };
