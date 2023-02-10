eb26ad77282464322b87ea7877dbdb71f3ccba7a
Daniil Reznikov <daniil@noorsoft-mobile.ru>
2023-02-10 16:58:10 +0200

    fix axiso api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,11 @@ import { USER_LOCALSTORAGE_KEY } from '../const/localstorage';
 
 export const api = axios.create({
    baseURL: __API__,
-   headers: {
-      authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-   },
+});
+
+api.interceptors.request.use((config) => {
+   if (config.headers) {
+      config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+   }
+   return config;
 });
