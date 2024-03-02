cb8c19fdbf764ce7b8ca27b047571fd3ebf993ac
Daniil <danrezni.pg@gmail.com>
2024-03-02 13:30:59 +0200

    feat:improve types for ReducerList

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -2,10 +2,10 @@ import { Reducer } from '@reduxjs/toolkit';
 import React, { ReactNode, useEffect } from 'react';
 import { useDispatch, useStore } from 'react-redux';
 
-import { ReduxStoreWithManager, StateSchemaKey } from '@/app/providers/StoreProvider';
+import { ReduxStoreWithManager, StateSchema, StateSchemaKey } from '@/app/providers/StoreProvider';
 
 export type ReducersList = {
-   [name in StateSchemaKey]?: Reducer;
+   [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>;
 };
 
 interface DynamicModuleLoaderProps {
